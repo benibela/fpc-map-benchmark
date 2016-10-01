e0a95569afed2184dda6b77f59d282b1e855511d
benibela <benito@benibela.de>
2016-10-01 19:19:02 +0200

    run script

---
+++ runhashbenchmark.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+  mkdir results
+  ./hashbenchmark --mode=list | while read map; do
+    ./runhashbenchmark.sh "$map"
+  done
+  exit;
+fi;
+
+
+if [[ -d /usr/share/hunspell/ ]]; then dicpath=/usr/share/hunspell/; 
+elif [[ -d /usr/share/myspell/ ]]; then dicpath=/usr/share/myspell/;
+else echo no dics; exit;
+fi
+dics="$dicpath/de_DE.dic:$dicpath/en_US.dic"
+
+echo "::" $1 $options
+readslist="0 1 20 1000"
+lenlist="4 32 200"
+for reads in $readslist; do
+  echo ./hashbenchmark --sources=$dics --filter=$1 --mode=multi-run --keycount=10 --memlimit=4096 results/$1.$reads.dics
+  for len in $lenlist; do
+    echo ./hashbenchmark --filter=$1 --mode=multi-run --keycount=10 --keylen=4 --memlimit=4096 results/$1.$reads.$len
+  done
+done
+#
