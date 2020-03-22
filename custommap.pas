cc77cfbaaca8449c78160357f062b74c9dceafc4
benibela <benito@benibela.de>
2020-03-22 13:28:58 +0100

    add custommap to git mirror

---
+++ custommap.pas
@@ -0,0 +1,20 @@
+//Here you can add your own map to quickly compare it to the existing maps
+unit custommap;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type TCustomMap = class()
+  //procedure add(const key: string; value: pointer);
+  //function get(const key: string): pointer;
+  //property defaultget[const key: string]: pointer write add read get; default;
+end;
+
+implementation
+
+end.
+
